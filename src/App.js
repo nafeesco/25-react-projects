d82b5285b435fe862a4c2a95515ab5709ed655e4
nafees <nafees.itsd@goldenharvestbd.com>
2024-05-09 17:07:39 +0600

    Added QR Code generator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import QRCodeGenerator from './components/qr-code-generator';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -18,7 +19,10 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image slider component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} />
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} /> */}
+
+      {/* QRCodeGenerator */}
+      <QRCodeGenerator/>
 
     </div>
   );
